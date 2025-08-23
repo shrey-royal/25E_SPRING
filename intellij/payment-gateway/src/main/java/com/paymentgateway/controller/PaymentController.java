20ecae3ea23c88ca7fe2572099a33dfbf819fe2e
Shrey K <srkkadia@gmail.com>
2025-08-23 15:17:16 +0530

    payment-gateway

--- intellij/payment-gateway/src/main/java/com/paymentgateway/controller/PaymentController.java
+++ intellij/payment-gateway/src/main/java/com/paymentgateway/controller/PaymentController.java
@@ -1,19 +1,29 @@
 package com.paymentgateway.controller;
 
-import com.paymentgateway.services.PaymentService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.paymentgateway.service.PaymentService;
+import net.authorize.api.contract.v1.ANetApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/payments")
+@RequestMapping("/payments")
 public class PaymentController {
-    private static final Logger logger = LoggerFactory.getLogger(PaymentController.class);
-
     @Autowired
     private PaymentService paymentService;
 
-    //
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @PostMapping
+    public ANetApiResponse creditCardPayment(@RequestBody Map<String, String> body) {
+        String creditCardNumber = body.get("creditCardNumber");
+        String expDate = body.get("expDate");
+        String email = body.get("email");
+
+        return paymentService.chargeCreditCard(creditCardNumber, 1999.0, email, expDate);
+    }
 }
--- intellij/payment-gateway/src/main/java/com/paymentgateway/enums/PaymentMethod.java
+++
@@ -1,7 +0,0 @@
-package com.paymentgateway.enums;
-
-public enum PaymentMethod {
-    CREDIT_CARD,
-    DEBIT_CARD,
-    BANK_ACCOUNT
-}
